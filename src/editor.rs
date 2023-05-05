ef959268a70cf6451db0859e71ddc470edf70fd9
mihsamusev <mih.samusev@gmail.com>
2023-05-05 15:08:51 +0200

    screen refresh

--- src/editor.rs
+++ src/editor.rs
@@ -1,25 +1,56 @@
-use std::io::{stdout, self};
+use std::io::{stdout, self, Write};
 use termion::raw::IntoRawMode;
 use termion::event::Key;
 use termion::input::TermRead;
 
 #[derive(Debug, Default)]
-pub struct Editor { }
+pub struct Editor {
+    should_quit: bool
+}
 
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Err(err) => panic!("deeznutz"),
-                Ok(key) => match key {
-                    Key::Ctrl('q')=> break,
-                    Key::Char(c) if c.is_control() => println!("{:?} \r", c as u8 ),
-                    _ =>  println!("{:?}\r", key)
-                }
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error)
             }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+            if self.should_quit {
+                println!("bye homie {}", termion::cursor::Goto(1, 1));
+                break;
+            }
+
         }
     }
+
+    fn process_keypress(&mut self) -> io::Result<()> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => {self.should_quit = true;} 
+            _ => ()
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(& self) -> io::Result<()> {
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        io::stdout().flush()
+    }
+}
+
+fn read_key() -> Result<Key, io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
+
+fn die(error: io::Error) {
+    panic!("dunno what happened, {:?}", error) 
 }
